c281b3641ce209ca4173e449cb7b2cd9dfdec86a
Crystal Contreras-Grossman & Jeyananthan Sivakumaran <student@epicodus.com>
2015-02-10 12:18:56 -0800

    adds lower_case prepositions, upper_case middle letters to downcase

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,13 @@
 var titleCase = function(title) {
-  var titleIntoWords = title.split(" ");
+  var titleIntoWords = title.toLowerCase().split(" ");
   var titleEquals = "";
+  var titlePrepositions = ["a" ,"an", "in", "the", "of", "and", "v", "v.", "vs"];
   titleIntoWords.forEach(function(word) {
-    titleEquals += word.charAt(0).toUpperCase() + word.slice(1) + " ";
+    if(titlePrepositions.indexOf(word) === -1) {
+      titleEquals += word.charAt(0).toUpperCase() + word.slice(1) + " ";
+    }else{
+      titleEquals += word.charAt(0) + word.slice(1) + " ";
+    }
   });
-  debugger;
-  return titleEquals;
+  return titleEquals.trim();
 };
--- spec/specs.js
+++ spec/specs.js
@@ -1,9 +1,21 @@
 describe('titleCase', function() {
-  // it("capitalizes the first letter of a word", function() {
-  //   expect(titleCase("cat")).to.equal("Cat");
-  // });
+  it("capitalizes the first letter of a word", function() {
+    expect(titleCase("cat")).to.equal("Cat");
+  });
 
   it("capitalizes the first letter of each word in a string", function () {
-    expect(titleCase("cat in the hat")).to.equal("Cat In The Hat");
+    expect(titleCase("cat in the hat")).to.equal("Cat in the Hat");
+  });
+
+  it("returns proper title case even if presented with letters in middle of word capitalized", function() {
+    expect(titleCase("cat IN the hAt")).to.equal("Cat in the Hat");
+  });
+
+  it("ignores prepositions and articles", function () {
+    expect(titleCase("Cat In The Hat")).to.equal("Cat in the Hat");
+  });
+
+  it("capitalizes the first letter of a title no matter what it is", function() {
+    expect(titleCase("a cat in the haT")).to.equal("A Cat in the Hat");
   });
 });
